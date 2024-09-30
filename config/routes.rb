64ad5fc69b3f49916060ec83883f6f07b9a945d9
njonge9 <frednjonge9@gmail.com>
2024-10-01 00:20:25 +0300

    Setup main route and signup form routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get "feed/show"
-  get "users/new"
-  get "users/create"
+  root "feed#show"
+  get "sign_up", to:"users#new"
+  post "sign_up", to:"users#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
