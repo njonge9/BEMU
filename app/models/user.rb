847be17a4909cb6ea8d2858ce72834b00c81dafd
njonge9 <frednjonge9@gmail.com>
2024-09-30 23:45:50 +0300

    Method to remove extra spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false }
     has_many :memberships, dependent: :destroy
     has_many :organizations, through: :memberships
+
+    before_validation :strip_extra_spaces
+
+    private
+
+    def strip_extra_spaces
+        self.name = self.name&.strip
+        self.email = self.email&.strip
+    end
 end
