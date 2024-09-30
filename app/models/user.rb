7073b06bde5f50787ce790839cdcfdba83463b04
njonge9 <frednjonge9@gmail.com>
2024-09-30 23:37:34 +0300

    connect models id

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     validates :name, presence: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, 
             uniqueness: { case_sensitive: false }
+    has_many :memberships, dependent: :destroy
+    has_many :organizations, through: :memberships
 end
