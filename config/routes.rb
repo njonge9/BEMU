1decce6e185283801659ea0ce6cb849cb431aeff
njonge9 <frednjonge9@gmail.com>
2024-10-01 13:30:23 +0300

    Add a login path to the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "feed#show"
+
   get "sign_up", to:"users#new"
   post "sign_up", to:"users#create"
+
+  get "login", to: "sessions#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
